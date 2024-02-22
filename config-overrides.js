e8542cf7eb3af769c9bceb41d802c4fce7b78b07
rizwanOT <muhammadrizwan@octathorn.com>
2024-02-22 11:58:04 +0500

    maximum file limit set

--- config-overrides.js
+++ config-overrides.js
@@ -1,23 +1,36 @@
 module.exports = function override(config, env) {
-    if (config.optimization && config.optimization.runtimeChunk) {
-      config.optimization.splitChunks = {
-        cacheGroups: {
-          default: false,
-        },
-      };
-  
-      config.optimization.runtimeChunk = false;
-    }
-  
-    if (config.plugins) {
-      config.plugins = config.plugins.map(plugin => {
-        if (plugin.constructor.name === 'GenerateSW') {
-          plugin.config.maximumFileSizeToCacheInBytes = 8000000; // 8MB
-        }
-  
-        return plugin;
-      });
-    }
-  
-    return config;
-  };
+  if (config.optimization && config.optimization.runtimeChunk) {
+    config.optimization.splitChunks = {
+      cacheGroups: {
+        default: false,
+      },
+    };
+
+    config.optimization.runtimeChunk = false;
+  }
+
+  if (config.plugins) {
+    config.plugins = config.plugins.map(plugin => {
+      if (plugin.constructor.name === 'WebpackManifestPlugin') {
+        plugin.opts.generate = (seed, files, entrypoints) => {
+          const manifestFiles = files.reduce((manifest, file) => {
+            manifest[file.name] = file.path;
+            return manifest;
+          }, seed);
+          const entrypointFiles = entrypoints.main.filter(
+            fileName => !fileName.endsWith('.map')
+          );
+
+          return {
+            files: manifestFiles,
+            entrypoints: entrypointFiles,
+          };
+        };
+      }
+
+      return plugin;
+    });
+  }
+
+  return config;
+};
